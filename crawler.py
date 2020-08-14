80f9d73cf13d9bdbd63da0198824ea0b0dac4dea
kmartin62 <martin.kostadinov@hotmail.com>
2020-08-14 23:57:07 +0200

    initial commit with labels and csv model files

---
+++ crawler.py
@@ -0,0 +1,8 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+from constants import PAGE_URL
+
+'''
+    TBA: Make three csv files, ads, cars, equipment and sellers. Later on add feedback comments on the ads csv 
+    and more info about the car model csv. 
+'''
